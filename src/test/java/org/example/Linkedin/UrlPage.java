e7b5ca73dd8683dd3e72b3713e6c0ae91af6cccf
MOHAMMAD SUMON <mohammad.sumon9897@gmail.com>
2024-01-12 22:55:38 +0600

    LinkedIn

--- src/test/java/org/example/Linkedin/UrlPage.java
+++ src/test/java/org/example/Linkedin/UrlPage.java
@@ -1,4 +1,4 @@
-package org.example.Yahoo;
+package org.example.Linkedin;
 
 import org.example.DriverSetup;
 import org.testng.annotations.Test;
@@ -7,7 +7,7 @@ public class UrlPage extends DriverSetup {
 
     @Test
     public void OpenUrl() throws InterruptedException {
-        browser.get("https://login.yahoo.com/?.src=ym&lang=en-GB&done=https%3A%2F%2Fmail.yahoo.com%2F%3Fguce_referrer%3DaHR0cHM6Ly93d3cuZ29vZ2xlLmNvbS8%26guce_referrer_sig%3DAQAAANTK5ZtLUct0atEJEgRv_XQJQC9RwtI9SaxSJklBIFOkQDAakoIjT67-SLUfIA6a3grBGqscoJ0dolsnrI074ndt6mrjVWTv9PHjO4S_EfieZgwpHNL15MyDgrJDBkwzpZ5T_b-AWnyY1Rixe63q4JbzId5kdHUEC2sYrgcjim8-");
+        browser.get("https://bd.linkedin.com/");
         Thread.sleep(2000);
 
         System.out.println(browser.getTitle());
